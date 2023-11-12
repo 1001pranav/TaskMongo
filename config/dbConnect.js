411bed5b539fe4097d137feb7a281473e9cca3e6
1001Pranav <npranavr@gmail.com>
2023-11-12 11:11:00 +0530

    Updated connection

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -3,12 +3,12 @@ import "dotenv/config";
 
 const CONN = null;
 
-export function DB_CONNECT () {
+export async function  DB_CONNECT () {
     const MONGO_DB = process.env.MONGO_CONFIG;
 
     if ( !MONGO_DB || typeof MONGO_DB !== "string" ) {
         throw new Error("Please Specify a DB configuration");
     }
 
-    return mongoose.createConnection(MONGO_DB);
+    return await mongoose.createConnection(MONGO_DB);
 }
